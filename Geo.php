9c602137292764f44297ce5738d5c2a001518523
Marius Hoch <hoo@online.de>
2014-09-26 03:39:58 +0200

    Specify 'license-name' => 'GPL-2.0+' for MediaWiki

--- Geo.php
+++ Geo.php
@@ -28,6 +28,7 @@ if ( defined( 'MEDIAWIKI' ) ) {
 		),
 		'url' => 'https://github.com/DataValues/Geo',
 		'description' => 'Geographical value objects, parsers and formatters',
+		'license-name' => 'GPL-2.0+'
 	);
 }
 
