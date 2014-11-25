53a19ad5bf1115191099be26d1b7c32603d3f7aa
Jan Zerebecki <jan.wikimedia@zerebecki.de>
2014-11-25 17:47:10 +0100

    Release 1.1.4
    actually increment version number

--- Geo.php
+++ Geo.php
@@ -15,7 +15,7 @@ if ( defined( 'DATAVALUES_GEO_VERSION' ) ) {
 	return 1;
 }
 
-define( 'DATAVALUES_GEO_VERSION', '1.1.3' );
+define( 'DATAVALUES_GEO_VERSION', '1.1.4' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	$GLOBALS['wgExtensionCredits']['datavalues'][] = array(
