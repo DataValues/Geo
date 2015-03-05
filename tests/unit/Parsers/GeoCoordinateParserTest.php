8cbf82b4a69af898f878fe1268c7842a6e12c93d
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-03-05 15:33:30 +0100

    Deprecate getFormatterClass in favor of getInstance

--- tests/unit/Parsers/GeoCoordinateParserTest.php
+++ tests/unit/Parsers/GeoCoordinateParserTest.php
@@ -22,7 +22,7 @@ class GeoCoordinateParserTest extends StringValueParserTest {
 	 * @deprecated since 0.3, just use getInstance.
 	 */
 	protected function getParserClass() {
-		return 'DataValues\Geo\Parsers\GeoCoordinateParser';
+		throw new \LogicException( 'Should not be called, use getInstance' );
 	}
 
 	/**
