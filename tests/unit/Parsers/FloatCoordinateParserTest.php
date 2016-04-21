375612b12c8f4cbc5ff3806d0649cd0061dc3095
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-04-21 18:19:16 +0200

    More specific deprecation comments

--- tests/unit/Parsers/FloatCoordinateParserTest.php
+++ tests/unit/Parsers/FloatCoordinateParserTest.php
@@ -19,7 +19,7 @@ use ValueParsers\Test\StringValueParserTest;
 class FloatCoordinateParserTest extends StringValueParserTest {
 
 	/**
-	 * @deprecated since 0.3, just use getInstance.
+	 * @deprecated since DataValues Common 0.3, just use getInstance.
 	 */
 	protected function getParserClass() {
 		throw new \LogicException( 'Should not be called, use getInstance' );
