baae57073c839b90295badfcd2a617d3f6f45f09
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-05-31 12:31:31 +0200

    Make use of the …::class feature

--- Geo.php
+++ Geo.php
@@ -33,9 +33,9 @@ if ( defined( 'MEDIAWIKI' ) ) {
 }
 
 // Aliases introduced in 1.0
-class_alias( 'DataValues\Geo\Values\LatLongValue', 'DataValues\LatLongValue' );
-class_alias( 'DataValues\Geo\Values\GlobeCoordinateValue', 'DataValues\GlobeCoordinateValue' );
+class_alias( DataValues\Geo\Values\LatLongValue::class, 'DataValues\LatLongValue' );
+class_alias( DataValues\Geo\Values\GlobeCoordinateValue::class, 'DataValues\GlobeCoordinateValue' );
 
 // Aliases introduced in 2.0
-class_alias( 'DataValues\Geo\Formatters\LatLongFormatter', 'DataValues\Geo\Formatters\GeoCoordinateFormatter' );
-class_alias( 'DataValues\Geo\Parsers\LatLongParser', 'DataValues\Geo\Parsers\GeoCoordinateParser' );
+class_alias( DataValues\Geo\Formatters\LatLongFormatter::class, 'DataValues\Geo\Formatters\GeoCoordinateFormatter' );
+class_alias( DataValues\Geo\Parsers\LatLongParser::class, 'DataValues\Geo\Parsers\GeoCoordinateParser' );
--- tests/unit/Values/GlobeCoordinateValueTest.php
+++ tests/unit/Values/GlobeCoordinateValueTest.php
@@ -23,7 +23,7 @@ class GlobeCoordinateValueTest extends DataValueTest {
 	 * @return string
 	 */
 	public function getClass() {
-		return 'DataValues\Geo\Values\GlobeCoordinateValue';
+		return GlobeCoordinateValue::class;
 	}
 
 	public function validConstructorArgumentsProvider() {
