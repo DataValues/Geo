a3b78d27a763af5bd63a28ed02a8812e5e17dfef
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-10-01 07:51:14 +0200

    Rename tests

--- tests/unit/Formatters/LatLongFormatterTest.php
+++ tests/unit/Formatters/LatLongFormatterTest.php
@@ -703,16 +703,14 @@ class LatLongFormatterTest extends TestCase {
 		];
 	}
 
-	public function testNoFloatIssuesWhenFormattingToFloat() {
-		// Regression test for https://github.com/JeroenDeDauw/Maps/issues/474
+	public function testRoundingWithDefaultPrecisionWhenFormattingAsFloat() {
 		$this->assertSame(
 			'52.144444444444, -0.46666666666667',
 			( new LatLongFormatter() )->format( new LatLongValue( 52.1445,-0.466722 ) )
 		);
 	}
 
-	public function testNoFloatIssuesWhenFormattingToDecimalDegrees() {
-		// Regression test for https://github.com/JeroenDeDauw/Maps/issues/474
+	public function testRoundingWithDefaultPrecisionWhenFormattingAsDecimalDegrees() {
 		$this->assertSame(
 			'52.1361°, -0.4219°',
 			$this->newDecimalDegreeFormatter()->format( new LatLongValue( 52.136,-0.422 ) )
