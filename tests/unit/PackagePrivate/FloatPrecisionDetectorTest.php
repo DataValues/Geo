ca5fd961300377a43c280e23a32db6e21229483e
Jeroen De Dauw <jeroendedauw@gmail.com>
2019-07-22 02:36:12 +0200

    Migrate DD precision test

--- tests/unit/PackagePrivate/FloatPrecisionDetectorTest.php
+++ tests/unit/PackagePrivate/FloatPrecisionDetectorTest.php
@@ -5,6 +5,7 @@ declare( strict_types = 1 );
 namespace Tests\DataValues\Geo\PackagePrivate;
 
 use DataValues\Geo\PackagePrivate\FloatPrecisionDetector;
+use DataValues\Geo\Parsers\DdCoordinateParser;
 use DataValues\Geo\Values\LatLongValue;
 use PHPUnit\Framework\TestCase;
 
@@ -47,6 +48,35 @@ class FloatPrecisionDetectorTest extends TestCase {
 		yield [ new LatLongValue( -1.00000001, -2.00000001 ), 0.00000001 ];
 		yield [ new LatLongValue( -1.000000001, -2.000000001 ), 1 ];
 		yield [ new LatLongValue( -1.555555555, -2.555555555 ), 0.00000001 ];
+	}
+
+	/**
+	 * @dataProvider decimalDegreeProvider
+	 */
+	public function testDecimalDegreePrecisionDetection( string $coordinate, float $expectedPrecision ) {
+		$latLong = ( new DdCoordinateParser() )->parse( $coordinate );
+
+		$this->assertSame(
+			$expectedPrecision,
+			( new FloatPrecisionDetector() )->detectPrecision( $latLong )->toFloat()
+		);
+	}
+
+	public function decimalDegreeProvider() {
+		yield [ '10° 20°', 1 ];
+		yield [ '1° 2°', 1 ];
+		yield [ '1.3° 2.4°', 0.1 ];
+		yield [ '1.3° 20°', 0.1 ];
+		yield [ '10° 2.4°', 0.1 ];
+		yield [ '1.35° 2.46°', 0.01 ];
+		yield [ '1.357° 2.468°', 0.001 ];
+		yield [ '1.3579° 2.468°', 0.0001 ];
+		yield [ '1.00001° 2.00001°', 0.00001 ];
+		yield [ '1.000001° 2.000001°', 0.000001 ];
+		yield [ '1.0000001° 2.0000001°', 0.0000001 ];
+		yield [ '1.00000001° 2.00000001°', 0.00000001 ];
+		yield [ '1.000000001° 2.000000001°', 1 ];
+		yield [ '1.555555555° 2.555555555°', 0.00000001 ];
 	}
 
 }
